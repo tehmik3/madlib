039df4bca87bad0be34b0895efba0ccb6b8d3e98
tehmi khan <tehmikhan32@gmail.com>
2025-04-11 19:26:54 +0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,26 @@
+noun = input("noun: ")
+adj = input("adj: ") 
+noun1 = input("noun1: ")
+noun2 = input("noun2: ")
+adj1 = input("adj1: ")
+adj2 = input("adj2: ") 
+adj3 = input("adj3: ")
+noun3 = input("noun3: ")
+noun4 = input("noun4: ")
+adj4 = input("adj4: ")
+adj5 = input("adj5: ")
+noun5 = input("noun5: ") 
+noun6 = input("noun6: ")
+noun7 = input("noun7: ")
+noun8  = input("noun8: ")
+adj6 = input("adj6: ")
+noun9 = input("noun9: ")
+
+
+madlib = f"{noun} is home to {adj} {noun1} like {noun2}, where the sea {adj1} \ncreates a {adj2} escape, and the {adj3} streets of {noun3}, filled with \nhistory and {noun6}. The city offers a {adj4} {noun8} of modernity and \n{noun4}, from the {adj5} {noun5} to the {adj6} markets of \n{noun9}."
+
+
+print(madlib)
+
+
+
